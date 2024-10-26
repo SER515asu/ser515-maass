45992dd650c76c92a22209d8b830c1844ebde8b4
Mrunal2148 <mkapure@asu.edu>
2024-10-25 18:21:39 -0700

    task 54

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SimulationPanel.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SimulationPanel.java
@@ -1,7 +1,10 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.BlockerManager;
 import com.groupesan.project.java.scrumsimulator.mainpackage.state.SimulationStateManager;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.BaseComponent;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -13,6 +16,8 @@ public class SimulationPanel extends JPanel implements BaseComponent {
     private SimulationStateManager simulationStateManager;
     private JButton startSimulationButton;
     private JButton stopSimulationButton;
+    private JButton showBlockersButton;
+    private JButton resolveBlockerButton;
 
     /** Simulation Panel Initialization. */
     protected SimulationPanel(SimulationStateManager simulationStateManager) {
@@ -24,35 +29,63 @@ public class SimulationPanel extends JPanel implements BaseComponent {
     public void init() {
         startSimulationButton = new JButton("Start Simulation");
         stopSimulationButton = new JButton("Stop Simulation");
+        showBlockersButton = new JButton("Resolve Blockers");
+        resolveBlockerButton = new JButton("Resolve Blockers First!");
 
         stopSimulationButton.setVisible(false);
+        showBlockersButton.setVisible(false);
+        BlockerManager blockerManager = new BlockerManager();
 
-        startSimulationButton.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        simulationStateManager.startSimulation();
-                        JOptionPane.showMessageDialog(null, "Simulation started!");
-                        updateButtonVisibility();
-                    }
-                });
+        startSimulationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                simulationStateManager.startSimulation();
+                JOptionPane.showMessageDialog(null, "Simulation started!");
+                updateButtonVisibility();
+                showBlockersButton.setVisible(true);
+                showBlockersButton.setBackground(Color.RED);
+                revalidate();
+                repaint();
+            }
+        });
 
-        stopSimulationButton.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        simulationStateManager.stopSimulation();
-                        JOptionPane.showMessageDialog(null, "Simulation stopped!");
-                        updateButtonVisibility();
-                    }
-                });
+        stopSimulationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+                if(blockerManager.isBlockerListEmpty()){
+                    simulationStateManager.stopSimulation();
+                    JOptionPane.showMessageDialog(null, "Simulation stopped!");
+                    updateButtonVisibility();
+                    showBlockersButton.setVisible(false);
+                    revalidate();
+                    repaint();
+                }
+                else {
+                    resolveBlockerButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            simulationStateManager.startSimulation();
+                            JOptionPane.showMessageDialog(null, "Resolve blockers first!");
+                        }
+                    });
+                }
+            }
+        });
+
+        showBlockersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                blockerManager.showBlockersPanel();
+            }
+        });
         add(startSimulationButton);
         add(stopSimulationButton);
+        add(showBlockersButton);
+
     }
 
     private void updateButtonVisibility() {
-        // Show/hide buttons based on the simulation state
         if (simulationStateManager.isRunning()) {
             stopSimulationButton.setVisible(true);
             startSimulationButton.setVisible(false);
