17ae866f57d97ad444bef651c1c5abcce3eab4ee
Mrunal2148 <mkapure@asu.edu>
2024-10-31 18:32:51 -0700

    test cases for BlockerManager class

---
+++ src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/impl/BlockerManagerTest.java
@@ -0,0 +1,37 @@
+package com.groupesan.project.java.scrumsimulator.mainpackage.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+
+public class BlockerManagerTest {
+    private BlockerManager blockerManager;
+
+    @BeforeEach
+    public void setupBlockers() {
+        blockerManager = new BlockerManager();
+    }
+
+    @Test
+    public void testAddBlocker() {
+        blockerManager.addBlocker("Blocker x");
+        assertTrue(blockerManager.blockers.contains("Blocker x"));
+    }
+
+    @Test
+    public void testResolveBlocker() {
+        blockerManager.resolveBlocker("Blocker 1");
+        assertFalse(blockerManager.blockers.contains("Blocker 1"));
+    }
+
+    @Test
+    public void testIsBlockerListEmptyWhenNotEmpty() {
+        assertFalse(blockerManager.isBlockerListEmpty());
+    }
+
+    @Test
+    public void testIsBlockerListEmptyWhenEmpty() {
+        blockerManager.blockers.clear();
+        assertTrue(blockerManager.isBlockerListEmpty());
+    }
+}
